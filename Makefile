7d8e5d39c4a7c9e877acf406b9e21ace03b57cbf
Nick Buonincontri <nbuonin@gmail.com>
2018-09-17 22:11:48 -0400

    Initial steps, bootstrapping project

---
+++ Makefile
@@ -0,0 +1,5 @@
+runserver:
+	npm run serve
+
+clean:
+	rm -rf node_modules
---
+++ src/CentralLimitGraph.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+class CentralLimitGraph extends Component {
+    constructor() {
+        super();
+        this.state = {}
+    }
+    render() {
+        return (
+            <h2>Central limit graph</h2>
+        )
+    }
+}
+
+export default CentralLimitGraph;
